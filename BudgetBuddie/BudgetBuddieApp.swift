5f53ef528f7b812cb68f0091ae238366766fae53
Adam Duflo <adduflo@gmail.com>
2025-12-22 15:01:08 -0800

    Initial Commit

---
+++ BudgetBuddie/BudgetBuddieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddieApp.swift
+//  BudgetBuddie
+//
+//  Created by Adam Duflo on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBuddieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetBuddieTests/BudgetBuddieTests.swift
@@ -0,0 +1,16 @@
+//
+//  BudgetBuddieTests.swift
+//  BudgetBuddieTests
+//
+//  Created by Adam Duflo on 12/22/25.
+//
+
+import Testing
+
+struct BudgetBuddieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
