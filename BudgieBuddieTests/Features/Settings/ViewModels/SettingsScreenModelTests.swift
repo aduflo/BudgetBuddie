79f1ad362a3abf832c1952945a752753843f7f0a
aduflo <adduflo@gmail.com>
2026-02-16 15:27:49 -0800

    buttoned up SettingsScreenModelTests

--- BudgieBuddieTests/Features/Settings/ViewModels/SettingsScreenModelTests.swift
+++ BudgieBuddieTests/Features/Settings/ViewModels/SettingsScreenModelTests.swift
@@ -9,11 +9,65 @@ import Foundation
 import Testing
 @testable import BudgieBuddie
 
+@MainActor
 struct SettingsScreenModelTests {
     // MARK: - defaultSpendTrendViewpoint
-    // MARK: - monthlyAllowance
-    // MARK: - setMonthlyAllowance()
-    // MARK: - warningThreshold
-    // MARK: - setWarningThreshold()
-    // MARK: - displayVersion
+    @Test func test_defaultSpendTrendViewpoint_update_alsoUpdates_settingsService() {
+        // Setup
+        let settingsService = MockSettingsService()
+        let vm = SettingsScreenModel(
+            settingsService: settingsService,
+            currencyFormatter: CurrencyFormatter()
+        )
+        
+        // Pre-verification
+        #expect(vm.defaultSpendTrendViewpoint == .spendAllowance)
+        #expect(settingsService.defaultSpendTrendViewpoint == .spendAllowance)
+        
+        // Scenario
+        let spendTrendViewpoint: SpendTrendViewpoint = .remainingOverspend
+        vm.defaultSpendTrendViewpoint = spendTrendViewpoint
+        
+        // Verification
+        #expect(vm.defaultSpendTrendViewpoint == spendTrendViewpoint)
+        #expect(settingsService.defaultSpendTrendViewpoint == spendTrendViewpoint)
+    }
+    
+    // MARK: - monthlyAllowance / setMonthlyAllowance()
+    @Test func test_monthlyAllowance() {
+        // Setup
+        let vm = SettingsScreenModel(
+            settingsService: MockSettingsService(),
+            currencyFormatter: CurrencyFormatter()
+        )
+        
+        // Pre-verification
+        #expect(vm.monthlyAllowance == 0.0)
+        
+        // Scenario
+        let monthlyAllowance: Decimal = 13.37
+        vm.setMonthlyAllowance(monthlyAllowance)
+        
+        // Verification
+        #expect(vm.monthlyAllowance == monthlyAllowance)
+    }
+    
+    // MARK: - warningThreshold / setWarningThreshold()
+    @Test func test_warningTreshold() {
+        // Setup
+        let vm = SettingsScreenModel(
+            settingsService: MockSettingsService(),
+            currencyFormatter: CurrencyFormatter()
+        )
+        
+        // Pre-verification
+        #expect(vm.warningThreshold == 0.0)
+        
+        // Scenario
+        let warningThreshold: Double = 13.37
+        vm.setWarningThreshold(warningThreshold)
+        
+        // Verification
+        #expect(vm.warningThreshold == warningThreshold)
+    }
 }
